93f845c9cf3ef6526cb10277c45b0848eb06c67f
Karnll Schultze <karnellschultz@gmail.com>
2020-03-06 13:22:08 +0100

    fixed footer position

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,68 +1,94 @@
-import Nav from './Nav';
-import styled from 'styled-components';
+import Nav from "./Nav";
+import styled from "styled-components";
 
 interface props {
-	children: any;
+  children: any;
 }
 
 const StyledLayout = styled.div`
-	color: ${({ theme }) => theme.colors.text};
-	background: ${({ theme }) => theme.colors.body};
-	min-height: 100vh;
-	min-width: 100vw;
+  color: ${({ theme }) => theme.colors.text};
+  background: ${({ theme }) => theme.colors.body};
+  min-height: 100vh;
+  min-width: 100vw;
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-	h1,
-	h2,
-	h3,
-	h4 {
-		color: ${({ theme }) => theme.colors.primary};
-	}
+  h1,
+  h2,
+  h3,
+  h4 {
+    color: ${({ theme }) => theme.colors.primary};
+  }
 
-	a {
-		color: ${({ theme }) => theme.colors.primary};
-		text-decoration: none;
-		transition: color 0.15s ease;
-	}
-	a:hover {
-		color: ${({ theme }) => theme.colors.primaryHover};
-		text-decoration: underline;
-	}
-	footer {
-		position: sticky;
-		background: ${({ theme }) => theme.colors.darkBackground};
-		width: 100%;
-		height: 100px;
-		border-top: 1px solid grey;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		font-size: 12px;
-		margin-top: 4rem;
-	}
-	footer img {
-		margin-left: 0.5rem;
-	}
-	footer a {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+  a {
+    color: ${({ theme }) => theme.colors.primary};
+    text-decoration: none;
+    transition: color 0.15s ease;
+  }
+  a:hover {
+    color: ${({ theme }) => theme.colors.primaryHover};
+    text-decoration: underline;
+  }
+  footer {
+    position: sticky;
+    background: ${({ theme }) => theme.colors.darkBackground};
+    width: 100%;
+    height: 100px;
+    border-top: 1px solid grey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 12px;
+    margin-top: -100px;
+  }
+  footer img {
+    margin-left: 0.5rem;
+  }
+  footer a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const StyledFooter = styled.footer`
+  position: sticky;
+  width: 100%;
+  height: 100px;
+  margin-top: -100px;
+  font-size: 12px;
+  border-top: 1px solid grey;
+
+  background: ${({ theme }) => theme.colors.darkBackground};
+  color: ${({ theme }) => theme.colors.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    margin-left: 0.5rem;
+  }
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export default function Layout({ children }: props) {
-	return (
-		<StyledLayout>
-			<Nav />
-			<main>{children}</main>
-			<footer>
-				<p>
-					<em>🌍Personal blog of Karnell Schultz 🌍</em>
-				</p>
-			</footer>
-		</StyledLayout>
-	);
+  return (
+    <>
+      <StyledLayout>
+        <Nav />
+        <main>{children}</main>
+      </StyledLayout>
+      <StyledFooter>
+        <p>
+          <em>🌍Personal blog of Karnell Schultz 🌍</em>
+        </p>
+      </StyledFooter>
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,42 @@
-import Post from '../components/Post';
-import styled from 'styled-components';
-import HeadTag from '../components/HeadTag';
-import { Section } from '../components/StyledComps';
+import Post from "../components/Post";
+import styled from "styled-components";
+import HeadTag from "../components/HeadTag";
+import { Section } from "../components/StyledComps";
 
 const ArticleSection = styled(Section)`
-	margin-top: 2rem;
+  margin-top: 2rem;
 `;
 
 export const Grid = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around;
-	align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
 
-	padding-bottom: 2rem;
+  padding-bottom: 2rem;
 
-	div {
-		padding-bottom: 2rem;
-	}
+  div {
+    padding-bottom: 2rem;
+  }
 `;
 
 const Home = () => (
-	<>
-		<HeadTag pageName="🔥 Home" />
-		<Section>
-			<h1>Karnell Schultz</h1>
-			<p className="description">
-				🤙🏾 I'm a Javascript developer interested in creating tools to
-				democrotize the web. I create{' '}
-				<a href="https://github.com/KarnellSchultz">open source</a>
-				&nbsp;projects and write about my experiences in tech.
-			</p>
-			<a href="/about">More about me &rarr;</a>
-		</Section>
-		<ArticleSection>
-			<Post />
-		</ArticleSection>
-		{/* <GridSection>
-			<Grid>
-				<div>
-					<ArticleCard
-						heading={'Hot new box'}
-						body={'This is cool new content'}
-					/>
-				</div>
-				<div>
-					<ArticleCard
-						heading={'Hot new box'}
-						body={'This is cool new content'}
-					/>
-				</div>
-				<div>
-					<ArticleCard
-						heading={'Hot new box'}
-						body={'This is cool new content'}
-					/>
-				</div>
-				<div>
-					<ArticleCard
-						heading={'Hot new box'}
-						body={'This is cool new content'}
-					/>
-				</div>
-				<div>
-					<ArticleCard
-						heading={'Hot new box'}
-						body={'This is cool new content'}
-					/>
-				</div>
-			</Grid>
-		</GridSection> */}
-	</>
+  <>
+    <HeadTag pageName="🔥 Home" />
+    <Section>
+      <h1>Karnell Schultz</h1>
+      <p className="description">
+        🤙🏾 I'm a Javascript developer interested in creating tools to
+        democrotize the web. I create{" "}
+        <a href="https://github.com/KarnellSchultz">open source</a>
+        &nbsp;projects and write about my experiences in tech.
+      </p>
+      <a href="/about">More about me &rarr;</a>
+    </Section>
+    <ArticleSection>
+      <Post />
+    </ArticleSection>
+  </>
 );
 
 export default Home;
