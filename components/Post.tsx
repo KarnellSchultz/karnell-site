d3edf5675e41860763d602ecd3e9b24eac61b344
Karnll Schultze <karnellschultz@gmail.com>
2020-03-09 16:38:18 +0100

    added WWW post, fixed li styles

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,70 +1,76 @@
-import styled from 'styled-components';
-import Link from 'next/link';
-import { StyledPostRow } from '../components/StyledComps';
+import styled from "styled-components";
+import Link from "next/link";
+import { StyledPostRow } from "../components/StyledComps";
 
 const StyledHeading = styled.article`
-	display: flex;
+  display: flex;
 
-	align-items: center;
-	button {
-		margin-left: 2rem;
-	}
+  align-items: center;
+  button {
+    margin-left: 2rem;
+  }
 `;
 
 const StyledList = styled.ul`
-	list-style-type: none;
-	padding: 0;
-	li {
-		list-style: none;
-	}
+  list-style-type: none;
+  padding: 0;
+  li {
+    list-style: none;
+  }
 `;
 
 const PostDetails: {
-	id: number;
-	title: string;
-	date: string;
-	link: string;
+  id: number;
+  title: string;
+  date: string;
+  link: string;
 }[] = [
-	{
-		id: 1,
-		title: '😅 A New Blog? What is this, 2006? ✍🏽',
-		date: 'March 2020',
-		link: '/articles/whyblog',
-	},
-	{
-		id: 0,
-		title: '💁🏾 A case for Learning Markdown 🔽',
-		date: 'March 2020',
-		link: '/articles/markdownguide',
-	},
+  {
+    id: 2,
+    title: "🥳 WWW.howImadeMySite.com 🥳",
+    date: "March 2020",
+    link: "/articles/howimademysite"
+  },
+  {
+    id: 1,
+    title: "😅 A New Blog? What is this, 2006? ✍🏽",
+    date: "March 2020",
+    link: "/articles/whyblog"
+  },
+  {
+    id: 0,
+    title: "💁🏾 A case for Learning Markdown 🔽",
+    date: "March 2020",
+    link: "/articles/markdownguide"
+  }
 ];
 //They appear in the order listed top to bottom
 
 const PostRow = (
-	PostDetails: { id: number; title: string; date: string; link: string }[],
+  PostDetails: { id: number; title: string; date: string; link: string }[]
 ): any => {
-	return PostDetails.map(row => {
-		return (
-			<li key={row.id}>
-				<Link href={row.link}>
-					<StyledPostRow>
-						<h3> {row.title} </h3>
-					</StyledPostRow>
-				</Link>
-			</li>
-		);
-	});
+  return PostDetails.map(row => {
+    return (
+      <li key={row.id}>
+        <Link href={row.link}>
+          <StyledPostRow>
+            <h3> {row.title} </h3>
+          </StyledPostRow>
+        </Link>
+      </li>
+    );
+  });
 };
 
 export default function Post() {
-	return (
-		<>
-			<StyledHeading>
-				<h2>Blog Posts</h2>
-				{/* <Button main={true} link="/articles" text="View All"></Button> */}
-			</StyledHeading>
-			<p>A collection of my thoughts and expeiences.</p>
-			<StyledList>{PostRow(PostDetails)}</StyledList>
-		</>
-	);
+  return (
+    <>
+      <StyledHeading>
+        <h2>Blog Posts</h2>
+        {/* <Button main={true} link="/articles" text="View All"></Button> */}
+      </StyledHeading>
+      <p>A collection of my thoughts and expeiences.</p>
+      <StyledList>{PostRow(PostDetails)}</StyledList>
+    </>
+  );
 }
---
+++ pages/articles/howimademysite.tsx
@@ -0,0 +1,10 @@
+import Howimademysite from "../../content/howimademysite.mdx";
+import { StyledBlogPost } from "../../components/StyledComps";
+
+export default function markdownGuide() {
+  return (
+    <StyledBlogPost>
+      <Howimademysite />
+    </StyledBlogPost>
+  );
+}
