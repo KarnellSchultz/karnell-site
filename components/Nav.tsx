e9c949376495459a819c8bd10c607c1d73d12a33
KarnellSchultz <KarnellSchultz@gmail.com>
2020-03-12 10:56:48 +0100

    refactored nav css, fixed some oddness

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,10 +4,11 @@ import styled from 'styled-components';
 import { MainStyledButton } from '../components/Button';
 
 const StyledNav = styled.nav`
-	@media (max-width: 600px) {
+	@media (max-width: 900px) {
 		/* background: red; */
-		width: 100%;
+		width: 100vw;
 		display: flex;
+
 		ul {
 			width: 100%;
 			display: flex;
@@ -22,13 +23,49 @@ const StyledNav = styled.nav`
 		}
 	}
 
-	background: green;
+	@media (max-width: 567px) {
+		padding: 0;
+		max-width: 100vw;
+
+		ul {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		li {
+			width: 100%;
+			padding: 0 0 0 0;
+			margin: 0 0 0 0;
+		}
+		li:last-of-type {
+			margin: 0 0 0 0;
+			padding: 0 0 0 0;
+		}
+	}
+
+	/* background: green; */
 	width: 100%;
 	display: flex;
-	flex-direction: row;
+	justify-content: center;
+	margin: 1rem;
 
 	ul {
+		width: 100%;
+		max-width: 860px;
+		display: flex;
+		flex-direction: row;
 		list-style: none;
+	}
+	li {
+		display: flex;
+		justify-content: center;
+		margin-right: 2rem;
+	}
+	li:last-of-type {
+		justify-content: flex-end;
+		/* width: 68%; */
+		width: 100%;
+		padding-right: 2rem;
 	}
 `;
 
