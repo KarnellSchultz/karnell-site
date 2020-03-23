fef89cad57dc68a1827907967aa10f996f312e00
KarnellSchultz <KarnellSchultz@gmail.com>
2020-03-23 10:03:40 +0100

    update yarn and nodemodules

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
