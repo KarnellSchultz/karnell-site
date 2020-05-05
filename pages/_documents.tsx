a4b6da448efdfbc6574d9ddc1f60db973e87d787
Karnll Schultze <karnellschultz@gmail.com>
2020-05-05 11:53:24 +0200

    working on styled comps bug

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -1,10 +1,11 @@
-import Document, { DocumentContext } from 'next/document';
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
+
     try {
       ctx.renderPage = () =>
         originalRenderPage({
