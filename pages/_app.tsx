596e8e37fb2bc708bb49c982a88dd7c4cd77ddaa
Karnll Schultze <karnellschultz@gmail.com>
2020-05-06 10:07:58 +0200

    trying adding styles.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import App from 'next/app';
 import Layout from '../components/Layout';
 
+import '../styles.css';
+
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
