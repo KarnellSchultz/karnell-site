b54d089992036939d555c4e34d5a215e3fca9c6b
Karnll Schultze <karnellschultz@gmail.com>
2020-08-18 16:15:57 +0200

    fix home button styles

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,17 +1,6 @@
 import Post from 'components/Post';
 import ArticleCard from 'components/ArticleCard';
-import styled from 'styled-components';
 import Layout from 'components/Layout';
-
-const Grid = styled.div`
-  display: flex;
-  flex: wrap;
-  justify-content: space-between;
-
-  div {
-    padding: 0.6rem;
-  }
-`;
 
 export default function articles() {
   return (
@@ -19,20 +8,6 @@ export default function articles() {
       <Layout pageName={'📰 Articles'}>
         <section>
           <Post></Post>
-          <Grid>
-            <div>
-              <ArticleCard
-                heading={'Smart stuff here'}
-                body={'Wow im so good at this'}
-              />
-            </div>
-            <div>
-              <ArticleCard
-                heading={'Markdown is cool'}
-                body={'Dang this is a cool component'}
-              />
-            </div>
-          </Grid>
         </section>
       </Layout>
     </>
