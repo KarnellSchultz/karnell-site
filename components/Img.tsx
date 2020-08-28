b5f40cfc2ac4048ff3f8dfe39746690f2bd4afd3
Karnll Schultze <karnellschultz@gmail.com>
2020-08-28 23:13:55 +0200

    move static assets into public/

--- components/Img.tsx
+++ components/Img.tsx
@@ -1,14 +1,17 @@
 interface props {
-    link:string;
-    alt:string;
+  link: string;
+  alt: string;
 }
-function Img( {link = 'content/images/githubReadme.png', alt}:props ) {
-    console.log(link);
-    
-    return (
-        <section>
-            <img src={link} alt={alt}/>
-        </section>
-    )
+function Img({
+  link = 'content/images/githubReadme.png',
+  alt,
+}: props) {
+  console.log(link);
+
+  return (
+    <section>
+      <img src={link} alt={alt} width={300} />
+    </section>
+  );
 }
-export default Img
+export default Img;
