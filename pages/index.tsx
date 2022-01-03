d4ad99250e5b4b88a94fc4fc54d9bac6b92881c0
karnell schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-01-03 10:44:19 -0500

    update config and fix index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,33 @@ import { Projects } from 'components/Projects';
 import { Layout } from 'components/Layout';
 import { Post } from 'components/post';
 
-const Home = () => (
-  <Layout pageName={'Home'}>
-    <section>
-      <h1>Karnell Schultz</h1>
-      <p className="description">
-        {`🤙🏾 I'm a web developer interested in creating tools to
+const Home = () => {
+  return (
+    <Layout pageName={'Home'}>
+      <section>
+        <h1>Karnell Schultz</h1>
+        <p className="description">
+          {`🤙🏾 I'm a web developer interested in creating tools to
         democrotize the web. I create `}
-        <a href="https://github.com/KarnellSchultz">
-          {`open source`}
-        </a>
-        {` projects and write about my experiences in tech. `}
-      </p>
-      <Link href="/about">
-        <p>
-          <a>More about me &rarr;</a>
+          <a href="https://github.com/KarnellSchultz">
+            {`open source`}
+          </a>
+          {` projects and write about my experiences in tech. `}
         </p>
-      </Link>
-    </section>
-    <section>
-      <Projects />
-    </section>
-    <section>
-      <Post />
-    </section>
-  </Layout>
-);
+        <Link href="/about">
+          <p>
+            <a>More about me &rarr;</a>
+          </p>
+        </Link>
+      </section>
+      <section>
+        <Projects />
+      </section>
+      <section>
+        <Post />
+      </section>
+    </Layout>
+  );
+};
 
 export default Home;
