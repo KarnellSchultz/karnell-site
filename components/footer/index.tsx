51bd5c51c5454f8e2c9a84c26431e06176ce7b42
karnell schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-05-20 10:51:28 +0200

    swapping css for sc in nav

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,43 +2,64 @@ import Link from 'next/link';
 import { AiFillGithub } from 'react-icons/ai';
 import { SiNextDotJs } from 'react-icons/si';
 import { BsFillTriangleFill } from 'react-icons/bs';
+import styled from 'styled-components';
+
+const FooterButton = styled.button`
+  color: var(--text-main);
+  background: var(--nav-button-background);
+  font-weight: 400;
+  font-size: 1.1rem;
+  padding: 0.7rem 1rem;
+  margin: 0 0.25rem;
+  border-radius: var(--borderRadius);
+  letter-spacing: 0px;
+  border: none;
+  transition: 10ms background;
+
+  &:hover {
+    cursor: pointer;
+    color: var(--nav-text);
+    background: var(--nav-button-background-hover);
+    text-decoration: none;
+  }
+`;
 
 function Footer() {
   return (
     <section>
       <footer>
         <Link href="/">
-          <button className="footer-a" type="button">
+          <FooterButton type="button">
             Home
-          </button>
+          </FooterButton>
         </Link>
         <Link href="/blog">
-          <button className="footer-a" type="button">
+          <FooterButton type="button">
             Blog
-          </button>
+          </FooterButton>
         </Link>
 
         <Link href="/about">
-          <button className="footer-a" type="button">
+          <FooterButton type="button">
             About
-          </button>
+          </FooterButton>
         </Link>
       </footer>
       <footer>
         <a href="https://vercel.com/">
-          <button className="footer-a" type="button">
+          <FooterButton type="button">
             <BsFillTriangleFill size={32} />
-          </button>
+          </FooterButton>
         </a>
         <a href="https://nextjs.org/">
-          <button className="footer-a" type="button">
+          <FooterButton type="button">
             <SiNextDotJs size={32} />
-          </button>
+          </FooterButton>
         </a>
         <a href="https://github.com/KarnellSchultz/karnell-site">
-          <button className="footer-a" type="button">
+          <FooterButton type="button">
             <AiFillGithub size={32} />
-          </button>
+          </FooterButton>
         </a>
       </footer>
     </section>
