1c794c3712a94269d81bdc8f6af89e2059a08a05
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-10-15 21:58:55 +0200

    fixed layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,12 @@
 import { useEffect } from 'react'
-import styled from 'styled-components'
+import Head from 'next/head'
 import Prism from 'prismjs'
 import 'prismjs/components/prism-jsx.min'
 import 'prismjs/plugins/unescaped-markup/prism-unescaped-markup.min.js'
-import Head from 'next/head'
 
 import { Nav } from './Nav'
 import { Themes } from './Themes'
 import { Footer } from './footer'
-
-const StyledLayout = styled.section`
-  min-height: 100vh;
-  min-width: 100vw;
-  main {
-    margin-bottom: 4rem;
-  }
-`
 
 interface LayoutProps {
   children: React.ReactNode
@@ -25,10 +16,10 @@ interface LayoutProps {
   meta?: any
 }
 
-const usePrism = () =>
-  useEffect(() => {
-    Prism.highlightAll()
-  }, [])
+// const usePrism = () =>
+//   useEffect(() => {
+//     Prism.highlightAll()
+//   }, [])
 
 function Layout({
   children,
@@ -36,11 +27,10 @@ function Layout({
   blog = false,
   meta,
 }: LayoutProps) {
-  // useEffect(() => {
-  //   Prism.highlightAll()
-  // }, [])
-
-  usePrism()
+  // usePrism()
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
 
   return (
     <>
@@ -54,15 +44,15 @@ function Layout({
         />
       </Head>
       <Themes>
-        <StyledLayout>
+        <section className="h-full w-full">
           <Nav />
-          <main>{children}</main>
+          <main className="mb-16">{children}</main>
           {blog && (
             <footer>
               <h5>🌍Thanks for Reading🌍</h5>
             </footer>
           )}
-        </StyledLayout>
+        </section>
         <Footer />
       </Themes>
     </>
