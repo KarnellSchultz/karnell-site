163ada40403bed747507cfc42e575cab975d4dfc
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-04-19 07:19:29 +0200

    removed older projects from main page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,28 +14,12 @@ const Projects = () => {
             link={'https://capitals.nellzus.se/'}
             gitLink={'https://github.com/KarnellSchultz/capitals'}
             blurb={`A game to guess the worlds capitals`}></ProjectItem>
-          <ProjectItem
-            title={'TypeTypeType 🚤'}
-            link={'https://typetypetype.vercel.app/'}
-            gitLink={'https://github.com/KarnellSchultz/typetypetype'}
-            blurb={`An application to test your typing speed.`}></ProjectItem>
         </div>
-        <div>
-          <ProjectItem
-            title={'Berlin Weather ⛅️'}
-            gitLink={
-              'https://github.com/KarnellSchultz/berlin-weather-next'
-            }
-            link={'https://berlin-weather.nellzus.se/'}
-            blurb={'Good old weather app'}></ProjectItem>
-          <ProjectItem
-            title={'Myra Tracker 🐜'}
-            gitLink={
-              'https://github.com/KarnellSchultz/MyraTrackingApp'
-            }
-            link={'https://myratracker.firebaseapp.com/login.html'}
-            blurb={`A 'Jira style' application created with Vue.js.`}></ProjectItem>
-        </div>
+        <ProjectItem
+          title={'TypeTypeType 🚤'}
+          link={'https://typetypetype.vercel.app/'}
+          gitLink={'https://github.com/KarnellSchultz/typetypetype'}
+          blurb={`An application to test your typing speed.`}></ProjectItem>
       </div>
     </>
   )
